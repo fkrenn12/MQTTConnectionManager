47c8c33eb592040512b3f4dd45f2654770be29f2
Franz Krenn <fkrenn@nanet.at>
2020-05-29 05:36:56 +0200

    Detection of using the tunnel or not improoved

--- mqtt_connection.py
+++ mqtt_connection.py
@@ -76,9 +76,8 @@ class Mqtt:
                     finally:
                         del self.__tunnel
                 # checking need of ssh tunnel
-                if self.__host is not "" and \
-                        self.__ssh_pass is not "" and \
-                        self.__ssh_user is not "":
+                if self.__ssh_pass is not "" and self.__ssh_pass is not None and \
+                        self.__ssh_user is not "" and self.__ssh_user is not None:
                     self.__use_tunnel = True
                     self.__connection_manager_state = State.CREATING_TUNNEL
                 else:
